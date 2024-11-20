6deb9a5156a8338c649bd472e50ce7f92ea046a9
TasmiyaKhan8028 <155808841+TasmiyaKhan8028@users.noreply.github.com>
2024-11-20 05:58:45 -0800

    get method use

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,32 +1,17 @@
-
 import { ConnectDB } from "@/lib/db/ConnectDB";
 import { UserModels } from "@/lib/models/user";
 
-
 export async function GET(request) {
-
-    await ConnectDB()
-    console.log('mongoDB connect');
-    const users = await UserModels.find()
-    return Response.json(users, {status: 200})
+await ConnectDB()
+const users = await UserModels.find()
+return Response.json({masg: 'user fetch success full ', user : users, },{status: 200})
 }
  
-
-console.log('conneted');
-export async function POST(request) {
-    await ConnectDB();
-    
-    
-    const obj = await request.json();
-    let newUser = new UserModels(obj)
-    await newUser.save()
-    return Response.json(newUser, {status: 201})
-}
+ 
+ 
+export async function POST(request) {}
  
 export async function PUT(request) {}
  
 export async function DELETE(request) {}
  
-
- 
-        
--- src/lib/models/user.js
+++ src/lib/models/user.js
@@ -1,18 +1,28 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
+import mongoose from 'mongoose'
 
-const userSchema = new Schema({
-  fullName: String,
-  email: String,
-  password: String,
-  location: {
-    lat: Number,
-    long: Number,
-  },
-  profileImg: String,
-  addres: String,
-  bio: String,
-});
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        maxlength: [60, 'Name cannot be more than 60 characters'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+        lowercase: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v)
+            },
+            message: 'Please enter a valid email'
+        },
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: [6, 'Password must be at least 6 characters long'],
+    },
+}, { timestamps: true })
 
-
-export const UserModels = mongoose.model('Users', userSchema);
+export const UserModels = mongoose.models.User || mongoose.model('User', userSchema)
